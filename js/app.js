502681ac546f1314763a54d1ab37625cdbbbd1ba
rodrigosanchone <rodrigosanchonet@outlook.es>
2025-03-08 13:00:02 -0600

    add footer

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const iconoMenu = document.querySelector("#content-icon");
 const a = document.querySelectorAll(".navLink");
 const btn_close = document.querySelector(".content-close");
+const body = document.querySelector("body");
 iconoMenu.addEventListener("click", navegacionResponsive);
 a.forEach((enlace) => {
   enlace.addEventListener("click", close);
@@ -9,9 +10,11 @@ btn_close.addEventListener("click", close);
 function navegacionResponsive() {
   const navegacion = document.querySelector("#nav");
   navegacion.classList.toggle("mostrar");
+  body.classList.add("content-y-hidden");
 }
 
 function close() {
   const navegacion = document.querySelector("#nav");
   navegacion.classList.remove("mostrar");
+  body.classList.remove("content-y-hidden");
 }
